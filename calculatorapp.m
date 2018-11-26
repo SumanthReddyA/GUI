e59421121b1c9e6890c94af2669a6f30d0ae99a9
Sumanth Reddy <asreddy89@outlook.com>
2018-11-26 15:35:34 +0530

    designing GUI and developing code for calculator

---
+++ calculatorapp.m
@@ -0,0 +1,14 @@
+function c = calculatorapp(a,operation,b)
+switch operation
+    case '+'
+        c = a+b;
+    case '-'
+        c = a-b;
+    case '*'
+        c = a*b;
+    case '/'
+        c = a/b;
+end
+end
+
+
